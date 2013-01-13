e0f403090206f3f838d59b0573342a2a611fcec5
Leif Gensert <leif@gensert.de>
2013-01-13 18:17:54 +0100

    add sugar to maid

--- maid/rules.rb
+++ maid/rules.rb
@@ -1,34 +1,4 @@
-# Sample Maid rules file -- some ideas to get you started.
-#
-# To use, remove ".sample" from the filename, and modify as desired.  Test using:
-#
-#     maid clean -n
-#
-# **NOTE:** It's recommended you just use this as a template; if you run these rules on your machine without knowing what they do, you might run into unwanted results!
-#
-# Don't forget, it's just Ruby!  You can define custom methods and use them below:
-#
-#     def magic(*)
-#       # ...
-#     end
-#
-# If you come up with some cool tools of your own, please send me a pull request on GitHub!
-#
-# For more help on Maid:
-#
-# * Run `maid help`
-# * Read the README, tutorial, and documentation at https://github.com/benjaminoakes/maid#maid
-# * Ask me a question over email (hello@benjaminoakes.com) or Twitter (@benjaminoakes)
-
 Maid.rules do
-  rule 'Linux ISOs, etc' do
-    trash(dir('~/Downloads/*.iso'))
-  end
-
-  rule 'Linux applications in Debian packages' do
-    trash(dir('~/Downloads/*.deb'))
-  end
-
   rule 'Mac OS X applications in disk images' do
     trash(dir('~/Downloads/*.dmg'))
   end
@@ -41,11 +11,18 @@ Maid.rules do
     trash(found)
   end
 
-  # NOTE: Currently, only Mac OS X supports `downloaded_from`.
-  rule 'Old files downloaded while developing/testing' do
+  rule 'Old Downloads' do
     dir('~/Downloads/*').each do |path|
       if 1.week.since?(accessed_at(path))
-        trash(path)
+        remove(path)
+      end
+    end
+  end
+
+  rule 'Old Movies' do
+    dir('~/Movies/queuebrake/to_malter').each do |path|
+      if 3.weeks.since?(accessed_at(path))
+        remove(path)
       end
     end
   end
