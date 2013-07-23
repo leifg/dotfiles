2ad7244795423f083437cbcc348a9c02172caa6e
Leif Gensert <leif@propertybase.com>
2013-07-23 11:42:22 +0200

    change ruby version to 1.9

--- tmux-layouts/falcon-development.window.sh
+++ tmux-layouts/falcon-development.window.sh
@@ -1,6 +1,6 @@
 window_root "~/projects/falcon"
 new_window "development"
-run_cmd "rbenv shell 2.0.0-p247; bundle exec guard"
+run_cmd "rbenv shell 1.9.3-p429; bundle exec guard"
 split_v 50
 
 select_pane 1
