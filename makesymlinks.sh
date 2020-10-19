acf458bed9f55191f19ba28b5caeec552694e82c
Leif Gensert <leif@leif.io>
2020-10-19 15:56:16 -0700

    simplify symlinks script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# set -euo pipefail
+
 ############################
-# .make.sh
+# .makesymlinks.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
 read -d '' files << EOF
@@ -32,20 +33,9 @@ EOF
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
+  echo "Processing $file"
   echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/.$file
+  ln -sfn $dir/$file ~/.$file
 done
